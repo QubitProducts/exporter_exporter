4b6fcccc4603c4550392b472223ebbe54089ef9b
Linas Daneliukas <linas@daneliukas.eu>
2020-03-09 16:59:15 +0200

    Add metric for build_info

--- version.go
+++ version.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 var (
 	Version   = "unset"
@@ -8,7 +13,23 @@ var (
 	Branch    = "unset"
 	BuildUser = "unset"
 	BuildDate = "unset"
+	GoVersion = runtime.Version()
 )
+
+var (
+	buildInfo = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "build_info",
+			Help: "A metric with a constant '1' value labeled by version, revision, branch and goversion from which exporter_exporter was built.",
+		},
+		[]string{"version", "revision", "branch", "goversion"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(buildInfo)
+	buildInfo.WithLabelValues(Version, Revision, Branch, GoVersion).Set(1)
+}
 
 func versionStr() string {
 	return fmt.Sprintf("%s-%s (from %s, built by %s on %s)", Version, Revision, Branch, BuildUser, BuildDate)
