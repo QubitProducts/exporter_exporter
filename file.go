1ae8a3ed35daef64fc020b8773080d3148f52cbd
Tomas Ebenlendr <ebik@ucw.cz>
2023-03-25 14:06:21 +0100

    Use io.ReadAll instead of code copied from os.File.ReadFile.

--- file.go
+++ file.go
@@ -54,44 +54,13 @@ func readFileWithDeadline(path string, t time.Time) ([]byte, time.Time, error) {
 	defer f.Close()
 	f.SetDeadline(t)
 
-	var size int
 	if info, err := f.Stat(); err == nil {
-		size64 := info.Size()
-		if int64(int(size64)) == size64 {
-			size = int(size64)
-		}
 		if info.Mode().IsRegular() {
 			mtime = info.ModTime()
 		}
 	}
-	size++ // one byte for final read at EOF
-
-	// If a file claims a small size, read at least 512 bytes.
-	// In particular, files in Linux's /proc claim size 0 but
-	// then do not work right if read in small pieces,
-	// so an initial read of 1 byte would not work correctly.
-	if size < 512 {
-		size = 512
-	}
-
-	data := make([]byte, 0, size)
-	for {
-		if time.Now().After(t) {
-			return data, mtime, os.ErrDeadlineExceeded
-		}
-		if len(data) >= cap(data) {
-			d := append(data[:cap(data)], 0)
-			data = d[:len(data)]
-		}
-		n, err := f.Read(data[len(data):cap(data)])
-		data = data[:len(data)+n]
-		if err != nil {
-			if err == io.EOF {
-				err = nil
-			}
-			return data, mtime, err
-		}
-	}
+	data, err := io.ReadAll(f)
+	return data, mtime, err
 }
 
 var (
